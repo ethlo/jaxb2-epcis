187906b0415b8a712183e0ece028fcc1b2ff35fe
Morten Haraldsen <dev@ethlo.com>
2025-03-21 12:57:25 +0100

    Support proper XSD dateTime

--- src/main/java/com/ethlo/jaxb/DateConverter.java
+++ src/main/java/com/ethlo/jaxb/DateConverter.java
@@ -2,31 +2,56 @@ package com.ethlo.jaxb;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
-public class DateConverter
-{
-    private DateConverter()
-    {
+public class DateConverter {
+    private static boolean xsdStandard;
+
+    private DateConverter() {
     }
 
-    public static LocalDate parseDate(String date)
-    {
+    /**
+     * If set to true, the dateTime values will only contain millis if they are non-zero.
+     * If set to false, the dateTime will always output millis, even if they are all zero.
+     *
+     * @param xsdStandard True if you want to omit millis if they are all zero (default is true)
+     */
+    public static void setUseXsdStandard(boolean xsdStandard) {
+        DateConverter.xsdStandard = xsdStandard;
+    }
+
+    public static LocalDate parseDate(String date) {
         return Optional.ofNullable(date).map(LocalDate::parse).orElse(null);
     }
 
-    public static OffsetDateTime parseDateTime(String dateTime)
-    {
+    public static OffsetDateTime parseDateTime(String dateTime) {
         return Optional.ofNullable(dateTime).map(OffsetDateTime::parse).orElse(null);
     }
 
-    public static String printDate(LocalDate date)
-    {
-        return Optional.ofNullable(date).map(LocalDate::toString).orElse(null);
+    // Print LocalDate to String (XML Schema 1.1 format: yyyy-MM-dd)
+    public static String printDate(LocalDate date) {
+        return Optional.ofNullable(date)
+                .map(d -> d.format(DateTimeFormatter.ISO_LOCAL_DATE))
+                .orElse(null);
     }
 
-    public static String printDateTime(OffsetDateTime dateTime)
-    {
-        return Optional.ofNullable(dateTime).map(OffsetDateTime::toString).orElse(null);
+    // Print OffsetDateTime to String (XML Schema 1.1 format: yyyy-MM-dd'T'HH:mm:ss.SSSXXX)
+    public static String printDateTime(OffsetDateTime dateTime) {
+        return xsdStandard ? xsdStandard(dateTime) : alwaysMillis(dateTime);
+    }
+
+    // Print OffsetDateTime to String (XML Schema 1.1 format: yyyy-MM-dd'T'HH:mm:ss.SSSXXX)
+    private static String xsdStandard(OffsetDateTime dateTime) {
+        return Optional.ofNullable(dateTime)
+                .map(d -> d.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+                .orElse(null);
+    }
+
+    // Print OffsetDateTime to String with always showing milliseconds (XML Schema 1.1 format: yyyy-MM-dd'T'HH:mm:ss.SSSXXX)
+    private static String alwaysMillis(OffsetDateTime dateTime) {
+        return Optional.ofNullable(dateTime)
+                .map(d -> d.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")))
+                .orElse(null);
     }
 }
